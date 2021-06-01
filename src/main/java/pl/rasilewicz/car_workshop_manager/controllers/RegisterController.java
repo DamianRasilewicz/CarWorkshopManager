71c591b70c45672d9b3a7f493faca3b011c45042
DamianRasilewicz <rasil754@gmail.com>
2021-06-01 14:42:01 +0200

    Added registration form (but it must be changed)

---
+++ src/main/java/pl/rasilewicz/car_workshop_manager/controllers/RegisterController.java
@@ -0,0 +1,28 @@
+package pl.rasilewicz.car_workshop_manager.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.rasilewicz.car_workshop_manager.entities.User;
+
+@Controller
+public class RegisterController {
+
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping("/registration")
+    public String registerForm(Model model){
+        model.addAttribute("user", new User());
+        return "registrationPage/registrationForm";
+    }
+
+    @PostMapping("/registration")
+    public String registerFormFilled(Model model){
+
+        
+        return "registrationPage/registrationForm";
+    }
+}
