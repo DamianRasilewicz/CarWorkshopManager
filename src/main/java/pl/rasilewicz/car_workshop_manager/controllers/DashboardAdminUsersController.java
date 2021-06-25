6f05b74ef9f6f4597ed34faa7c1117228413ea56
DamianRasilewicz <rasil754@gmail.com>
2021-06-25 21:35:37 +0200

    User edit in admin dashboard works

--- src/main/java/pl/rasilewicz/car_workshop_manager/controllers/DashboardAdminUsersController.java
+++ src/main/java/pl/rasilewicz/car_workshop_manager/controllers/DashboardAdminUsersController.java
@@ -3,7 +3,13 @@ package pl.rasilewicz.car_workshop_manager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.rasilewicz.car_workshop_manager.entities.Role;
 import pl.rasilewicz.car_workshop_manager.entities.User;
+import pl.rasilewicz.car_workshop_manager.services.RoleServiceImpl;
 import pl.rasilewicz.car_workshop_manager.services.UserServiceImpl;
 import java.util.List;
 
@@ -11,9 +17,11 @@ import java.util.List;
 public class DashboardAdminUsersController {
 
     private final UserServiceImpl userService;
+    private final RoleServiceImpl roleService;
 
-    public DashboardAdminUsersController(UserServiceImpl userService) {
+    public DashboardAdminUsersController(UserServiceImpl userService, RoleServiceImpl roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping("/dashboard/admin/users")
@@ -22,5 +30,30 @@ public class DashboardAdminUsersController {
         model.addAttribute("userList", userList);
 
         return "dashboardPages/admin/users";
+    }
+
+    @GetMapping("/dashboard/admin/users/edit")
+    public String userDetails (@RequestParam Integer id, Model model){
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+
+        return "dashboardPages/admin/userEdit";
+    }
+
+    @PostMapping("/dashboard/admin/users/edit")
+    public String userDetailsChanged (@ModelAttribute("selectedUserId") Integer selectedUserId, @ModelAttribute("newRole") String newRole,
+                                      @ModelAttribute("newEnabled") Boolean newEnabled, RedirectAttributes redirectAttributes){
+
+        User editedUser = userService.findUserById(selectedUserId);
+
+        Role selectedRole = roleService.findRoleByName(newRole);
+
+        editedUser.setRole(selectedRole);
+        editedUser.setEnabled(newEnabled);
+        userService.save(editedUser);
+
+        redirectAttributes.addAttribute("id", selectedUserId);
+
+        return "redirect:/dashboard/admin/users/edit?success";
     }
 }
